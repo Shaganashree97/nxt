9bc5b186237e054e3d7e1da784c965a534b72d6d
Shaganashree97 <shaganashree.pl@gmail.com>
2024-08-04 13:35:16 +0530

    body font color

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const jetbrainsMono = JetBrains_Mono({ 
+  subsets: ["latin"], 
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"], 
+  variable: '--font-jetbrainsMono',
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jetbrainsMono.variable}>{children}</body>
     </html>
   );
 }
