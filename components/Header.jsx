b917b8e2d36b7761fbbf50d569a485739b9f59a4
Shaganashree97 <shaganashree.pl@gmail.com>
2024-08-04 14:34:40 +0530

    desktop nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+// components
+import Nav from "./Nav";
+
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
@@ -10,6 +13,13 @@ const Header = () => {
             <h1 className="text-4xl font-semibold">
                 Shagana<span className="text-accent">.</span></h1>
             </Link>
+
+
+            { /* desktop nav */}
+            <div className="hidden xl:flex">
+                <Nav />
+            </div>
+            
         </div>
     </header>
   )
